dbdd757f8088095294f4e3d9ddc0bd25ee877008
sanchezg7 <gsanc.tech@pm.me>
2026-01-03 16:02:59 -0500

    wip: docs for D1 interfacing and creating link

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -25,11 +25,11 @@ export const linksTrpcRoutes = t.router({
     }),
     // TRPC will provide, input, data, ctz
   createLink: t.procedure.input(createLinkSchema).mutation(async ({input, ctx}) => {
-      await createLink({
+      const id = await createLink({
           accountId: ctx.userInfo.userId,
           ...input,
       });
-    return "random-id";
+    return id;
   }),
   updateLinkName: t.procedure
     .input(
---
+++ packages/data-ops/src/db/README.md
@@ -0,0 +1,4 @@
+# DB
+
+[D1 Staging](https://dash.cloudflare.com/dba950c0645b0e21ff1ef9d7465012ac/workers/d1/databases/fe1a2999-90ed-456e-9908-ed53ac4bc8bf/metrics)
+[D1 Staging Query](https://dash.cloudflare.com/dba950c0645b0e21ff1ef9d7465012ac/workers/d1/databases/fe1a2999-90ed-456e-9908-ed53ac4bc8bf/studio)
