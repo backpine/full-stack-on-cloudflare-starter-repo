6067a49ccb8d1f040837a4ce8cdf3b5316caf42a
sanchezg7 <gsanc.tech@pm.me>
2026-01-29 16:10:23 -0500

    wip: adding some helper docs for cf headers for geolocation

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -2,12 +2,33 @@ import { Hono } from 'hono';
 
 export const App = new Hono<{Bindings: Env}>();
 
+App.get("/hello-world", async (c) => {
+	return c.json({
+		message: "Hello world"
+	})
+});
+
 /**
  * this is a dynamic id
  * c will give the contenxt for hono. It will give access to the env, request, response
  */
 App.get("/:id", async (c) => {
+	/**
+	 * We need to use the headers on the req
+	 * Hono will add some helper methods at this point
+	 * It can provide the c.req.raw from the cloudflare request
+	 */
+
+	const regularHeaders = c.req.raw.headers;
+	console.log(JSON.stringify(c.req.raw.headers))
+
+	/**
+	 * This has a lot of useful info that CF will provide. Check out cf.mock.jsonc for an actual example
+	 */
+	const cfHeaders = c.req.raw.cf;
+	console.log(JSON.stringify(cfHeaders))
 	return c.json({
-		message: "Hello world"
+		message: "updatem"
 	})
 });
+
