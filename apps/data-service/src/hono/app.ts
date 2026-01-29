9a625117fd9d8f86a044848405b5559f77007968
sanchezg7 <gsanc.tech@pm.me>
2026-01-29 16:33:23 -0500

    wip: got the link details endpoint running but running into db probs, need to configure that

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import {getLink} from "@repo/data-ops/queries/links";
 
 export const App = new Hono<{Bindings: Env}>();
 
@@ -8,34 +9,44 @@ App.get("/hello-world", async (c) => {
 	})
 });
 
-/**
- * this is a dynamic id
- * c will give the contenxt for hono. It will give access to the env, request, response
- */
-App.get("/:id", async (c) => {
-	/**
-	 * We need to use the headers on the req
-	 * Hono will add some helper methods at this point
-	 * It can provide the c.req.raw from the cloudflare request
-	 */
+// -----------
+// we are starting over with a more simple solution
+// /**
+//  * this is a dynamic id
+//  * c will give the contenxt for hono. It will give access to the env, request, response
+//  */
+// App.get("/:id", async (c) => {
+// 	/**
+// 	 * We need to use the headers on the req
+// 	 * Hono will add some helper methods at this point
+// 	 * It can provide the c.req.raw from the cloudflare request
+// 	 */
+//
+// 	const regularHeaders = c.req.raw.headers;
+// 	console.log(JSON.stringify(c.req.raw.headers))
+//
+// 	/**
+// 	 * This has a lot of useful info that CF will provide. Check out cf.mock.jsonc for an actual example
+// 	 */
+// 	const cfHeaders = c.req.raw.cf;
+// 	console.log(JSON.stringify(cfHeaders))
+//
+// 	const country = cfHeaders?.country;
+// 	const lat = cfHeaders?.latitude;
+// 	const lng = cfHeaders?.longitude;
+//
+// 	return c.json({
+// 		country,
+// 		lat,
+// 		lng
+// 	})
+// });
+//
 
-	const regularHeaders = c.req.raw.headers;
-	console.log(JSON.stringify(c.req.raw.headers))
 
-	/**
-	 * This has a lot of useful info that CF will provide. Check out cf.mock.jsonc for an actual example
-	 */
-	const cfHeaders = c.req.raw.cf;
-	console.log(JSON.stringify(cfHeaders))
-
-	const country = cfHeaders?.country;
-	const lat = cfHeaders?.latitude;
-	const lng = cfHeaders?.longitude;
-
-	return c.json({
-		country,
-		lat,
-		lng
-	})
-});
-
+App.get('/:id', async (c) => {
+	const id = c.req.param('id');
+	const linkInfoFromDb = await getLink(id)
+	console.log(linkInfoFromDb)
+	return c.json({...linkInfoFromDb})
+})
