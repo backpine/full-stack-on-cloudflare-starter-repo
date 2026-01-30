bd613d2ad2b0f41f673bd1f6c5387f6fe93ebca2
sanchezg7 <gsanc.tech@pm.me>
2026-01-30 17:27:18 -0500

    feat: queue

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -22,4 +22,18 @@ export default class DataService extends WorkerEntrypoint<Env> {
 	/**
 	 * There are other hooks like queue, scheduled etc for other types of compute primitives
 	 */
+
+	/**
+	 * This is the handler for the queue
+	 * @param batch
+	 */
+	async queue(batch: MessageBatch<unknown>) {
+		for (const message of batch.messages) {
+			console.log("Queue Event: ", message.body);
+
+			// you can manually ack the message
+			// message.ack()
+		}
+	}
 }
+
