ae6a280226e962f2f256e31634f926df7b133372
sanchezg7 <gsanc.tech@pm.me>
2026-01-31 11:34:15 -0500

    feat(browser-render): init browser rendering

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,17 +1,39 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
+import puppeteer from "@cloudflare/puppeteer";
 
 
 /**
- * This is to tap into the workflow engine
+ * This is to tap into the workflow engine. It's using the DestinationStatus... from the service bindings
  */
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-	async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
 
 		const collectedData = await step.do("Collect rendered destination page data", async () => {
-			console.log("Collecting rendered destination page data");
-			return {
-				"dummydata": "dummydata",
+			/**
+			 * This is the cloudflare way of standing up a browser
+			 */
+			const browser = await puppeteer.launch(this.env.VIRTUAL_BROWSER)
 
+			// now we can do other browser specific things
+			const page = await browser.newPage();
+			// make sure the service bindings updates to have url
+			const response = await page.goto(event.payload.destinationUrl);
+
+			await page.waitForNetworkIdle();
+
+			const bodyText = (await page.$eval('body', (el) => el.innerText)) as string;
+
+			// this is provided by puppeteer
+			const html = await page.content();
+
+			const status = response ? response.status() : 0;
+
+			await browser.close();
+
+			return {
+				bodyText,
+				html,
+				status
 			}
 		});
 		console.log(collectedData);
