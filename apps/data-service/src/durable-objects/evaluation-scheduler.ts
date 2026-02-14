0f8b95a3a0d3cacca528624c311234374e405ed8
sanchezg7 <gsanc.tech@pm.me>
2026-02-13 19:03:33 -0500

    wip: updates to scheduler for it to trigger in the future for the alarm entry point

--- apps/data-service/src/durable-objects/evaluation-scheduler.ts
+++ apps/data-service/src/durable-objects/evaluation-scheduler.ts
@@ -32,11 +32,30 @@ export class EvaluationScheduler extends DurableObject {
 		// we don't necessarily want to run the workflow each time, so we should use an alarm with a trigger condition that will instead invoke the workflow
 		// this.env.DESINATION_EVALUATION_WORKFLOW.create()
 
+		// a simple scheduler into the future
 		const alarm = await this.ctx.storage.getAlarm()
 		if(!alarm) {
 			const tenSeconds = Date.now() + 10000;
 			await this.ctx.storage.setAlarm(tenSeconds);
 		}
+	}
+
+	async alarm() {
+		console.log('Evaluation scheduler alarm triggered');
+		const clickData = this.clickData;
+		if(!clickData) {
+			// we don't expect this to happen, just guarding
+			throw new Error('No click data found');
+		}
+		// recall that we have to update service bindings ts file so that it's typed
+		// there are input props in a workflow that we can't get type hints because it can't pick it up, so we define it
+		await this.env.DESINATION_EVALUATION_WORKFLOW.create({
+			params: {
+				linkId: clickData?.linkId,
+				accountId: clickData.accountId,
+				destinationUrl: clickData.destinationUrl
+			}
+		})
 	}
 }
 
